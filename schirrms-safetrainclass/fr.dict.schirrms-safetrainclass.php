5db044266b2dfa3b4cebc1294386160998bd308c
Pascal SCHIRRMANN <pascal.schirrmann@sacem.fr>
2023-11-21 14:51:49 +0100

    ✨ First 'complete' version

--- schirrms-safetrainclass/fr.dict.schirrms-safetrainclass.php
+++ schirrms-safetrainclass/fr.dict.schirrms-safetrainclass.php
@@ -8,10 +8,13 @@
 
  Dict::Add('FR FR', 'French', 'Français', array(
 	// Dictionary entries go here
-	'Class:SafeTrain' => 'SAFe Train',
-	'Class:SafeTrain/Attribute:name' => 'SAFe Train Name',
-	'Class:SafeTrain/Attribute:s_train' => 'SAFe Train associated Tag',
-	'Class:SafeTrain/Attribute:org_id' => 'Organization ID',
-	'Class:SafeTrain/Attribute:org_name' => 'Organization name',
+	'Class:SafeTrain' => 'Train SAFe',
+  'Class:SafeTrain/Attribute:name' => 'nom du train SAFe',
+  'Class:SafeTrain/Attribute:name+' => 'Devrait être le même nom que le nom du train dans les étiquettes',
+  'Class:SafeTrain/Attribute:s_train' => 'Code de l\'étiquette associé à ce train SAFe',
+  'Class:SafeTrain/Attribute:s_train+' => 'DOIT être une étiquette valide !',
+  'Class:SafeTrain/Attribute:org_id' => 'Code de l\'organisation',
+  'Class:SafeTrain/Attribute:org_name' => 'Nom de l\'organisation',
+	'Class:SafeTrain/Attribute:VMObsolescencedashboard' => 'Obsolescence des OS des Machines Virtuelles',
 ));
 ?>
