dec8298211e9890e6564d4fa2682927741efdf36
Pascal SCHIRRMANN <pascal.schirrmann@sacem.fr>
2023-11-21 09:50:43 +0100

    🚧 Commit before rename

---
+++ schirrms-safetrainclass/fr.dict.schirrms-safetrain.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Localized data
+ *
+ * @copyright   Copyright (C) 2023 Pascal Schirrmann
+ * @license     http://opensource.org/licenses/AGPL-3.0
+ */
+
+ Dict::Add('FR FR', 'French', 'Français', array(
+	// Dictionary entries go here
+	'Class:SafeTrain' => 'SAFe Train',
+	'Class:SafeTrain/Attribute:name' => 'SAFe Train Name',
+	'Class:SafeTrain/Attribute:s_train' => 'SAFe Train associated Tag',
+	'Class:SafeTrain/Attribute:org_id' => 'Organization ID',
+	'Class:SafeTrain/Attribute:org_name' => 'Organization name',
+));
+?>
